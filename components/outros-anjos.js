fee00fcffc44697546dacecac5671b4b869dc7b0
Pedro Maravilha <pedro.maravilha@wundermanthompson.com>
2020-10-20 15:53:27 +0100

    Novo teste

--- components/outros-anjos.js
+++ components/outros-anjos.js
@@ -9,14 +9,14 @@ export default function PostPreview({
     <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
       <div>
         <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-          <p className="text-lg leading-relaxed mb-4">{nome}</p>
+          <p className="text-lg leading-relaxed mb-4">Nome: {nome}</p>
           <p className="text-lg leading-relaxed mb-4">{genero}</p>
-          <p className="text-lg leading-relaxed mb-4">{idade}</p>
+          <p className="text-lg leading-relaxed mb-4">Idade: {idade}</p>
         </h3>
       </div>
       <div>
-        <p className="text-lg leading-relaxed mb-4">{brinquedo}</p>
-        <p className="text-lg leading-relaxed mb-4">{talhaDaRoupa}</p>
+        <p className="text-lg leading-relaxed mb-4">Prenda: {brinquedo}</p>
+        <p className="text-lg leading-relaxed mb-4">Roupa para {talhaDaRoupa}</p>
       </div>
     </div>
   )
