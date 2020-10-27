a1036c276119b93cb88d26921a2552e2894d4fae
Pedro Maravilha <pedro.maravilha@wundermanthompson.com>
2020-10-27 21:39:29 +0000

    4 Colunas

--- components/outros-anjos.js
+++ components/outros-anjos.js
@@ -6,18 +6,17 @@ export default function AnjoPreview({
   idade,
 }) {
   return (
-    <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
-      <div>
-        <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-          <p className="text-lg leading-relaxed mb-4">Nome: {nome}</p>
-          <p className="text-lg leading-relaxed mb-4">{genero}</p>
-          <p className="text-lg leading-relaxed mb-4">Idade: {idade}</p>
-        </h3>
+
+    <div>
+      <div className="text-center">
+        <p className="text-xl mx-auto leading-relaxed font-bold"> {nome}</p>
+        <div className="leading-relaxed mb-4">{idade}</div>
       </div>
-      <div>
-        <p className="text-lg leading-relaxed mb-4">Prenda: {brinquedo}</p>
-        <p className="text-lg leading-relaxed mb-4">Roupa para {talhaDaRoupa}</p>
+      <div className="md:grid text-base md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+          <p className="leading-relaxed mb-4">Prenda: {brinquedo}</p>
+          <p className="leading-relaxed mb-4">Roupa para {talhaDaRoupa}</p>
       </div>
     </div>
+
   )
 }
