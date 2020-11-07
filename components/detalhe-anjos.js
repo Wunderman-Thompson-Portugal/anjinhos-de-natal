b20b8652299eb62cc64a2efcb9dcca66a1094747
Pedro Maravilha <pedro.maravilha@wundermanthompson.com>
2020-11-07 00:22:58 +0000

    Código do Slack

--- components/detalhe-anjos.js
+++ components/detalhe-anjos.js
@@ -1,4 +1,5 @@
 import PostTitle from '@/components/post-title'
+import { CommerceLayer, AddToCartButton } from '@commercelayer/react-components'
 
 export default function DetalheAnjos({ nome, idade, brinquedo, talhaDaRoupa, genero }) {
   return (
@@ -15,7 +16,33 @@ export default function DetalheAnjos({ nome, idade, brinquedo, talhaDaRoupa, gen
             <p className="leading-relaxed mb-4">Roupa para {talhaDaRoupa}</p>
         </div>
       </div>
+      <CommerceLayer accessToken="eyJhbGciOiJIUzUxMiJ9.eyJvcmdhbml6YXRpb24iOnsiaWQiOjE5NTB9LCJhcHBsaWNhdGlvbiI6eyJpZCI6Mjc4NCwia2luZCI6InNhbGVzX2NoYW5uZWwiLCJwdWJsaWMiOnRydWV9LCJ0ZXN0Ijp0cnVlLCJleHAiOjE2MDQ2ODk0NjksIm1hcmtldCI6eyJpZCI6MzQwMywicHJpY2VfbGlzdF9pZCI6MzM3OSwic3RvY2tfbG9jYXRpb25faWRzIjpbMzc2M10sImdlb2NvZGVyX2lkIjpudWxsLCJhbGxvd3NfZXh0ZXJuYWxfcHJpY2VzIjpmYWxzZX0sInJhbmQiOjAuNjM5NTc5Njg0OTk4MDMwMn0.HhuI3ew0ixN0o00zuP6P_OT2MQ-KCodLrdLqIlmmdhDuRDYTzusAYsiMWVYqRCF3h1oQYXBghQ8elgOxhx36CA" endpoint="https://anjinhosdenatal.commercelayer.io">
+      {/* ... child components */}
+      <AddToCartButton>
+        {CustomAddToCart}
+      </AddToCartButton>
+      </CommerceLayer>
     </div>
     </>
+  )
+}
+
+const CustomAddToCart = (props) => {
+  const classes = props.disabled ? 'opacity-50 cursor-not-allowed' : ''
+  const myClick = async () => {
+    const { success } = await props.handleClick()
+    if (success) {
+      location.href = 'https://anjinhosdenatal-escolher.netlify.app/wOBnhRaVWR'
+    }
+  }
+  return (
+    <button
+      id="add-to-bag"
+      className={`${classes} ${props.className}`}
+      onClick={myClick}
+      disabled={props.disabled}
+    >
+      Buy now
+    </button>
   )
 }
